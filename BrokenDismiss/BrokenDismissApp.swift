c594fa9929e1853d9021c59c97a3f567b59a29f9
Oliver Foggin <oliverfoggin@me.com>
2023-11-29 13:17:21 +0000

    Initial Commit

---
+++ BrokenDismiss/BrokenDismissApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokenDismissApp.swift
+//  BrokenDismiss
+//
+//  Created by Oliver Foggin on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrokenDismissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrokenDismiss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrokenDismiss
+//
+//  Created by Oliver Foggin on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
